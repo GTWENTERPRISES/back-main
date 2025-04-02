55a787147b24be79b214f4fad14d78dab203a086
GTWENTERPRISES <murdcofh@gmail.com>
2025-04-02 16:01:56 -0500

    vbvcb

--- back/settings.py
+++ back/settings.py
@@ -53,8 +53,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    
 }
 CORS_ORIGIN_ALLOW_ALL = True
 
